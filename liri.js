4cc1597a610545d6b75fdf45c6b3c94cd449a24e
daniel 'kirplink' reynolds <=>
2018-11-17 20:25:18 -0600

    basic functionality added

---
+++ liri.js
@@ -0,0 +1,71 @@
+require("dotenv").config();
+
+const Spotify = require("node-spotify-api");
+const keys = require("./keys");
+const axios = require("axios");
+const moment = require("moment");
+const spotify = new Spotify(keys.spotify);
+
+
+
+spotifyThisSong = (songName) => {
+    spotify.search({ type: 'track', query: songName }, function(err, data) {
+        if (err) {
+          return console.log('Error occurred: ' + err);
+        }
+       
+      console.log("Artist: " + data.tracks.items[0].album.artists[0].name);
+      console.log("Song Name: " + data.tracks.items[0].name); 
+      console.log("Preview: " + data.tracks.items[0].external_urls.spotify);
+      console.log("Album: " + data.tracks.items[0].album.name); 
+      });
+}
+
+movieThis = (movieName) => {
+    let queryUrl = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
+    
+    // console.log(queryUrl);
+    
+    axios.get(queryUrl).then(
+        function(response) {
+            console.log("Title: " + response.data.Title);
+            console.log("Release Year: " + response.data.Year);
+            console.log("IMDB Rating: " + response.data.Ratings[0].Value);
+            console.log("Rotten Tomatoes Rating: " + response.data.Ratings[1].Value);
+            console.log("Country: " + response.data.Country);
+            console.log("Language: " + response.data.Language);
+            console.log("Plot: " + response.data.Plot);
+            console.log("Actors: " + response.data.Actors);
+        }
+    );
+}
+
+concertThis = (artist) => {
+    let queryUrl = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"
+
+    console.log(queryUrl);
+
+    axios.get(queryUrl).then(
+        function(response) {
+            console.log(response.data[0].venue.name);
+            console.log(response.data[0].venue.city + ', ' + response.data[0].venue.region);
+            console.log(response.data[0].datetime);
+        }
+    );
+}
+
+// spotifyThisSong(process.argv[2]);
+
+switch(process.argv[2]) {
+    case 'spotify-this-song':
+        spotifyThisSong(process.argv[3])
+    break;
+    
+    case 'movie-this':
+        movieThis(process.argv[3])
+    break;
+    
+    case 'concert-this':
+        concertThis(process.argv[3])
+    break;
+}
